bd05c1cb73bb463ea84795e11cd4cf5c2d0ac5be
Anton <45577251+h1mal1a1@users.noreply.github.com>
2024-03-24 17:37:22 +0300

    Add files via upload
    Digital root is the recursive sum of all the digits in a number.
    
    Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
    Examples
        16  -->  1 + 6 = 7
       942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
    132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
    493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2

---
+++ SumOfDigits.cs
@@ -0,0 +1,11 @@
+﻿internal class SumOfDigits
+{
+    public static int DigitalRoot(long n)
+    {
+        string strNumb = n.ToString();
+        long tmpNumb = 0;
+        foreach(var symb in strNumb)
+            tmpNumb += long.Parse(symb.ToString());
+        return (tmpNumb.ToString().Length > 1) ? DigitalRoot(tmpNumb): (int)tmpNumb;
+    }
+}
