4ed1628cb745b7dcd0a22085030b753f5c15c4ae
Anton <45577251+h1mal1a1@users.noreply.github.com>
2024-03-25 20:48:53 +0300

    RegexValidatePINCode
    ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
    If the function is passed a valid PIN string, return true, else return false.
    
    Examples (Input --> Output)
    "1234"   -->  true
    "12345"  -->  false
    "a234"   -->  false

---
+++ RegexValidatePINCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FirstProject
+{
+    internal class RegexValidatePINCode
+    {
+        public static bool ValidatePin(string pin)
+        {
+            Regex re = new(@"[0-9]");
+            MatchCollection mc = re.Matches(pin);
+            if (mc.Count != pin.Length)
+                return false;
+            if(pin.Length != 4 || pin.Length != 6) 
+                return false;
+
+            return true;
+        }
+
+        public static void Main()
+        {
+            List<string> lstPins = ["1234"];
+            lstPins.ForEach(pin => { Console.WriteLine(ValidatePin(pin)); });
+        }
+    }
+}
