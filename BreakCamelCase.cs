74fc9f9784afa6eaee1264d42d400ab671388cb4
Anton <45577251+h1mal1a1@users.noreply.github.com>
2024-03-30 17:55:27 +0300

    Break camelCase
    Complete the solution so that the function will break up camel casing, using a space between words.
    
    Example
    "camelCasing"  =>  "camel Casing"
    "identifier"   =>  "identifier"
    ""             =>  ""

---
+++ BreakCamelCase.cs
@@ -0,0 +1,17 @@
+﻿namespace FirstProject
+{
+    internal class BreakCamelCase
+    {
+        public static string BreakCC(string str)
+        {
+            if (str.Length == 0)
+                return str;
+
+            string tmpStr = string.Empty;
+            foreach(char symb in str)
+                tmpStr += symb.ToString() == symb.ToString().ToUpper() ? $" {symb}" : symb;
+
+            return tmpStr;
+        }
+    }
+}
