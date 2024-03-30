14f14b3edf1e20d19d154a4bc2d154ce4502e9b5
Anton <45577251+h1mal1a1@users.noreply.github.com>
2024-03-31 01:41:45 +0300

    Detect Pangram
    A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
    
    Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.

---
+++ IsPangram.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstProject
+{
+    internal class IsPangram
+    {
+        private static bool IP(string str)
+        {
+            string alph = string.Empty;
+
+            foreach(var symb in str)
+            {
+                if (!char.IsLetter(symb))
+                    continue;
+                if (alph.Contains(symb.ToString().ToLower()))
+                    continue;
+                alph += symb.ToString().ToLower();
+            }
+            return (alph.Length == 26) ? true : false;
+        }
+    }
+}
