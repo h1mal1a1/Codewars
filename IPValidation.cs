3e139d0e88c710bc66be8e70ed2100df44e3e0f0
Anton <45577251+h1mal1a1@users.noreply.github.com>
2024-04-09 23:58:46 +0300

    IP Validation
    Write an algorithm that will identify valid IPv4 addresses in dot-decimal format. IPs should be considered valid if they consist of four octets, with values between 0 and 255, inclusive.
    
    Valid inputs examples:
    Examples of valid inputs:
    1.2.3.4
    123.45.67.89
    Invalid input examples:
    1.2.3
    1.2.3.4.5
    123.456.78.90
    123.045.067.089
    Notes:
    Leading zeros (e.g. 01.02.03.04) are considered invalid
    Inputs are guaranteed to be a single string
    
    Assert.AreEqual(true, Kata.IsValidIp("0.0.0.0"));
    Assert.AreEqual(true, Kata.IsValidIp("12.255.56.1"));
    Assert.AreEqual(true, Kata.IsValidIp("137.255.156.100"));
    Assert.AreEqual(false, Kata.IsValidIp(""));
    Assert.AreEqual(false, Kata.IsValidIp("abc.def.ghi.jkl"));
    Assert.AreEqual(false, Kata.IsValidIp("123.456.789.0"));
    Assert.AreEqual(false, Kata.IsValidIp("12.34.56"));
    Assert.AreEqual(false, Kata.IsValidIp("12.34.56.00"));
    Assert.AreEqual(false, Kata.IsValidIp("12.34.56.7.8"));
    Assert.AreEqual(false, Kata.IsValidIp("12.34.256.78"));
    Assert.AreEqual(false, Kata.IsValidIp("1234.34.56"));
    Assert.AreEqual(false, Kata.IsValidIp("pr12.34.56.78"));
    Assert.AreEqual(false, Kata.IsValidIp("12.34.56.78sf"));
    Assert.AreEqual(false, Kata.IsValidIp("12.34.56 .1"));
    Assert.AreEqual(false, Kata.IsValidIp("12.34.56.-1"));
    Assert.AreEqual(false, Kata.IsValidIp("123.045.067.089"));

---
+++ IPValidation.cs
@@ -0,0 +1,26 @@
+﻿namespace FirstProject
+{
+    internal class IPValidation
+    {
+        public static bool IsValidIp(string ipAddres)
+        {
+            var lstOCeans = ipAddres.Split('.');
+            if (lstOCeans.Length != 4) { return false; }
+            foreach (var oceans in lstOCeans)
+            {
+                if (oceans == null) return false;
+                var symbInOceans = oceans.ToCharArray();
+                if (symbInOceans.Length == 0 || symbInOceans.Length > 3) return false;
+                if (symbInOceans[0] == '0' && symbInOceans.Length > 1) return false;
+                foreach (char symb in symbInOceans)
+                {
+                    if (!char.IsDigit(symb))
+                        return false;
+                }
+                if (int.Parse(oceans) > 255 || int.Parse(oceans) < 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
