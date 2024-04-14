0b9d73afe5a9c62e2f1aeddcaf57613514789e9d
Anton <45577251+h1mal1a1@users.noreply.github.com>
2024-04-14 23:09:23 +0300

    Give me a Diamond
    Jamie is a programmer, and James' girlfriend. She likes diamonds, and wants a diamond string from James. Since James doesn't know how to make this happen, he needs your help.
    
    Task
    You need to return a string that looks like a diamond shape when printed on the screen, using asterisk (*) characters. Trailing spaces should be removed, and every line must be terminated with a newline character (\n).
    
    Return null/nil/None/... if the input is an even number or negative, as it is not possible to print a diamond of even or negative size.
    
    
    Examples
    A size 3 diamond:
    
     *
    ***
     *
    ...which would appear as a string of " *\n***\n *\n"
    
    A size 5 diamond:
    
      *
     ***
    *****
     ***
      *
    ...that is:
    
    "  *\n ***\n*****\n ***\n  *\n"

---
+++ Diamond.cs
@@ -0,0 +1,20 @@
+﻿namespace FirstProject
+{
+    internal class Diamond
+    {
+        public static string? PrintDiamonds(int n)
+        {
+            if ((n < 0) || n % 2 == 0)
+                return null;
+
+            string str = string.Empty;
+            for (int i = 1, j = (n / 2); j >= 0; i += 2, j--)
+                str += $"{new string(' ', j)}{new string('*', i)}\n";
+
+            for (int i = n - 2, j = 1; j <= (n / 2); i -= 2, j++)
+                str += $"{new string(' ', j)}{new string('*', i)}\n";
+
+            return str;
+        }
+    }
+}
