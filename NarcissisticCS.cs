9173bebab301e8125e7fef47f0b8214ee1679b1e
Anton <45577251+h1mal1a1@users.noreply.github.com>
2024-04-14 20:52:36 +0300

    Does my number look big in this?
    A Narcissistic Number (or Armstrong Number) is a positive number which is the sum of its own digits, each raised to the power of the number of digits in a given base. In this Kata, we will restrict ourselves to decimal (base 10).
    
    For example, take 153 (3 digits), which is narcissistic: 1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
    and 1652 (4 digits), which isn't: 1^4 + 6^4 + 5^4 + 2^4 = 1 + 1296 + 625 + 16 = 1938
    The Challenge:
    Your code must return true or false (not 'true' and 'false') depending upon whether the given number is a Narcissistic number in base 10.
    This may be True and False in your language, e.g. PHP.
    Error checking for text strings or other invalid inputs is not required, only valid positive non-zero integers will be passed into the function.

---
+++ NarcissisticCS.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstProject
+{
+    internal class NarcissisticCS
+    {
+        public static bool Narcissistic(int value)
+        {
+            double newNum = 0;
+            foreach (var numb in value.ToString())
+            {
+                int tmpNumb = 1;
+                for (int i = 0; i < value.ToString().Length; i++)
+                    tmpNumb *= int.Parse(numb.ToString());
+                newNum += tmpNumb;
+            }
+
+            return newNum == value;
+        }
+    }
+}
