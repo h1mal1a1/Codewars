8cff93cf262522ae4cd975bc02961c573c417fae
Anton <45577251+h1mal1a1@users.noreply.github.com>
2024-04-15 19:55:38 +0300

    Build a pile of Cubes
    Your task is to construct a building which will be a pile of n cubes. The cube at the bottom will have a volume of n^3 , the cube above will have volume of (n-1)^3 and so on until the top which will have a volume of 1^3. 
    You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
    The parameter of the function findNb (find_nb, find-nb, findNb, ...)  will be an integer m and you have to return the integer n such as n^3 + (n-1)^3 + (n-2)^3 + ... + 1^3 = m if such a n exists or -1 if there is no such n.
    Examples:
    findNb(1071225) --> 45
    findNb(91716553919377) --> -1

---
+++ ASum.cs
@@ -0,0 +1,25 @@
+﻿namespace FirstProject
+{
+    public class ASum
+    {
+        public static long BinaryPow(long numb, long exp)
+        {
+            long result = 1;
+            while (exp > 0)
+            {
+                if ((exp & 1) == 1)
+                    result *= numb;
+                numb *= numb;
+                exp >>= 1;
+            }
+            return result;
+        }
+        public static long FindNb(long m)
+        {
+            long n = 1, newValue = 0;
+            for (; newValue < m; n++)
+                newValue += BinaryPow(n, 3);
+            return (newValue == m) ? n - 1 : -1;
+        }
+    }
+}
