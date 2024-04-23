30143438249d5737cc16173f5d1996bb6de6dacb
Anton <45577251+h1mal1a1@users.noreply.github.com>
2024-04-23 22:37:08 +0300

    Adding Big Numbers
    We need to sum big numbers and we require your help.
    Write a function that returns the sum of two numbers. The input numbers are strings and the function must return a string.
    
    Example
    add("123", "321"); -> "444"
    add("11", "99");   -> "110"
    Notes
    The input numbers are big.
    The input is a string of only digits
    The numbers are positives

---
+++ AddingBigNumbers.cs
@@ -0,0 +1,40 @@
+﻿namespace FirstProject
+{
+    internal class AddingBigNumbers
+    {
+        public static string Add(string a, string b)
+        {
+            bool additionalUnit = false;
+            string result = string.Empty;
+
+            while (a.Length != b.Length)
+            {
+                if (a.Length > b.Length)
+                    b = b.Insert(0, "0");
+                else if (b.Length > a.Length)
+                    a = a.Insert(0, "0");
+            }
+
+            for (int i = a.Length - 1; i >= 0; i--)
+            {
+                int numb = int.Parse(a[i].ToString()) + int.Parse(b[i].ToString());
+                if (additionalUnit)
+                    numb += 1;
+
+                if (numb >= 10)
+                {
+                    numb %= 10;
+                    additionalUnit = true;
+                }
+                else
+                    additionalUnit = false;
+
+                result = result.Insert(0, numb.ToString());
+            }
+            if (additionalUnit)
+                result = result.Insert(0, "1");
+
+            return result;
+        }
+    }
+}
