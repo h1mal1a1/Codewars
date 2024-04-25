4a4278775f8f59dc105c5fbda3ac8139848dd530
Anton <45577251+h1mal1a1@users.noreply.github.com>
2024-04-25 19:01:21 +0300

    Count IP Addresses
    Implement a function that receives two IPv4 addresses, and returns the number of addresses between them (including the first one, excluding the last one).
    All inputs will be valid IPv4 addresses in the form of strings. The last address will always be greater than the first one.
    Examples
    * With input "10.0.0.0", "10.0.0.50"  => return   50 
    * With input "10.0.0.0", "10.0.1.0"   => return  256 
    * With input "20.0.0.10", "20.0.1.0"  => return  246

---
+++ CountIPAddresses.cs
@@ -0,0 +1,25 @@
+﻿namespace FirstProject
+{
+    internal class CountIPAddresses
+    {
+        public static long BinaryPow(long numb, long exp)
+        {
+            long result = 1;
+            while (exp > 0)
+            {
+                if ((exp & 1) == 1)
+                    result *= numb;
+                numb *= numb;
+                exp >>= 1;
+            }
+            return result;
+        }
+        public static long IpsBetween(string start, string end)
+        {
+            long result = 0;
+            for (int i = 0; i < 4; i++)
+                result += (int.Parse(end.Split('.')[i]) - int.Parse(start.Split('.')[i])) * BinaryPow(256, 3 - i);
+            return result;
+        }
+    }
+}
