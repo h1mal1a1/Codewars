d5bc4f68aa3444dbce3810d7855e7a918d63f642
Anton <45577251+h1mal1a1@users.noreply.github.com>
2024-04-26 23:08:23 +0300

    Number of trailing zeros of N!
    Write a program that will calculate the number of trailing zeros in a factorial of a given number.
    N! = 1 * 2 * 3 *  ... * N
    Be careful 1000! has 2568 digits...
    For more info, see: http://mathworld.wolfram.com/Factorial.html
    Examples
    zeros(6) = 1
    # 6! = 1 * 2 * 3 * 4 * 5 * 6 = 720 --> 1 trailing zero
    zeros(12) = 2
    # 12! = 479001600 --> 2 trailing zeros
    Hint: You're not meant to calculate the factorial. Find another way to find the number of zeros.

---
+++ NumberOfTrailingZerosOfN.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstProject
+{
+    internal class NumberOfTrailingZerosOfN
+    {
+        public static double TrailingZeros(int n)
+        {
+            int res = 0;
+            for(int i = 1; i <= (int)Math.Log(n, 5); i++)
+                res += n / (int)Math.Pow(5, i);
+
+            return res;
+        }
+    }
+}
