dedd6569042653ac023dd09fe53f652558728301
Anton <45577251+h1mal1a1@users.noreply.github.com>
2025-02-09 08:27:06 +0300

    Mean Square Error
    https://www.codewars.com/kata/51edd51599a189fe7f000015
    Complete the function that:
     - accepts two integer arrays of equal length
     - compares the value each member in one array to the corresponding member in the other
     - squares the absolute value difference between those two values
     - and returns the average of those squared absolute value difference between each member pair.
    Examples
    [1, 2, 3], [4, 5, 6]                      -->   9   because (9 + 9 + 9) / 3
    [10, 20, 10, 2], [10, 25, 5, -2]   -->  16.5 because (0 + 25 + 25 + 16) / 4
    [-1, 0], [0, -1]                          -->   1   because (1 + 1) / 2

---
+++ MeanSquareError.cs
@@ -0,0 +1,10 @@
+﻿namespace FirstProject
+{
+    internal class MeanSquareError
+    {
+        public static double Solution(int[] firstArray, int[] secondArray)
+        {
+            return (double)firstArray.Select((t, i) => (t - secondArray[i]) * (t - secondArray[i])).Sum() / firstArray.Length;
+        }
+    }
+}
