fcba8b2d112d8d9486aaf84237819dda3a03914d
Anton <45577251+h1mal1a1@users.noreply.github.com>
2025-02-11 08:26:59 +0300

    Simple Pig Latin
    https://www.codewars.com/kata/520b9d2ad5c005041100000f
    Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
    Examples
    Kata.PigIt("Pig latin is cool"); // igPay atinlay siay oolcay
    Kata.PigIt("Hello world !");     // elloHay orldway !

---
+++ SimplePigLatin.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace FirstProject
+{
+    internal class SimplePigLatin
+    {
+        internal static string TaskWithRegEx(string str)
+        {
+            string ptrnAlWrds = @"[^\s]\w*";
+            string ptrnWrdsWithoutSpecSymb = @"[^\s][A-z]\w*";
+
+            var allMtchs = new Regex(ptrnAlWrds, RegexOptions.IgnoreCase).Matches(str);
+            var mtchsOnlyWords = new Regex(ptrnWrdsWithoutSpecSymb, RegexOptions.IgnoreCase).Matches(str);
+
+            string[] words = new string[allMtchs.Count];
+            int counter = 0;
+
+            foreach(Match mtch in allMtchs) 
+            {
+                if (!mtchsOnlyWords.Select(m=>m.Value).Contains(mtch.Value))
+                    words[counter] = mtch.Value;
+                else
+                {
+                    string ptrnFChar = @"^.";
+                    var fL = new Regex(ptrnFChar, RegexOptions.IgnoreCase).Matches(mtch.Value).First();
+                    words[counter] = $"{Regex.Replace(mtch.Value, ptrnFChar, string.Empty)}{fL.Value}ay";
+                }
+                counter++;
+            }
+
+            return string.Join(' ',words);
+        }
+    }
+}
