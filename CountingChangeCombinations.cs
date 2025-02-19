69267138da031e9ac9283d0fa12edec0921e621f
Anton <45577251+h1mal1a1@users.noreply.github.com>
2025-02-19 21:48:09 +0300

    Counting Change Combinations
    https://www.codewars.com/kata/541af676b589989aed0009e7
    
    Write a function that counts how many different ways you can make change for an amount of money, given an array of coin denominations. For example, there are 3 ways to give change for 4 if you have coins with denomination 1 and 2: 1+1+1+1, 1+1+2, 2+2.
    The order of coins does not matter: 1+1+2 == 2+1+1
    Also, assume that you have an infinite amount of coins.
    Your function should take an amount to change and an array of unique denominations for the coins:
    CountCombinations(4, new[] {1,2}) // => 3
    CountCombinations(10, new[] {5,2,3}) // => 4
    CountCombinations(11, new[] {5,7}) //  => 0

---
+++ CountingChangeCombinations.cs
@@ -0,0 +1,25 @@
+﻿namespace FirstProject
+{
+    internal class CountingChangeCombinations
+    {
+        public static int CountCombinations(int money, int[] coins)
+        {
+            switch(coins.Length)
+            {
+                case > 0 when money == 0:
+                    return 1;
+                case 0 when money > 0:
+                    return 0;
+                case 1:
+                    return money % coins[0] != 0 ? 0 : 1;
+            }
+            var countCombination = 0;
+            for(var i = 0; i <= money / coins[0]; i++)
+            {
+                var res = CountCombinations(money - i * coins[0], coins.Skip(1).ToArray());
+                countCombination += res;
+            }
+            return countCombination;
+        }
+    }
+}
