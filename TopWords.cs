d3d05e009ae15b96f5ef464e3a2c0ad304dc7f1e
Anton <45577251+h1mal1a1@users.noreply.github.com>
2025-02-20 19:45:51 +0300

    Add files via upload
    https://www.codewars.com/kata/51e056fe544cf36c410000fb
    Write a function that, given a string of text (possibly with punctuation and line-breaks), returns an array of the top-3 most occurring words, in descending order of the number of occurrences.
    
    Assumptions:
    A word is a string of letters (A to Z) optionally containing one or more apostrophes (') in ASCII.
    Apostrophes can appear at the start, middle or end of a word ('abc, abc', 'abc', ab'c are all valid)
    Any other characters (e.g. #, \, / , . ...) are not part of a word and should be treated as whitespace.
    Matches should be case-insensitive, and the words in the result should be lowercased.
    Ties may be broken arbitrarily.
    If a text contains fewer than three unique words, then either the top-2 or top-1 words should be returned, or an empty array if a text contains no words.
    Examples:
    "In a village of La Mancha, the name of which I have no desire to call to
    mind, there lived not long since one of those gentlemen that keep a lance
    in the lance-rack, an old buckler, a lean hack, and a greyhound for
    coursing. An olla of rather more beef than mutton, a salad on most
    nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
    on Sundays, made away with three-quarters of his income."
    
    --> ["a", "of", "on"]
    
    
    "e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e"
    
    --> ["e", "ddd", "aa"]
    
    
    "  //wont won't won't"
    
    --> ["won't", "wont"]
    Bonus points (not really, but just for fun):
    Avoid creating an array whose memory footprint is roughly as big as the input text.
    Avoid sorting the entire array of unique words.

---
+++ TopWords.cs
@@ -0,0 +1,26 @@
+﻿namespace FirstProject
+{
+    internal class TopWords
+    {
+        public static List<string> Top3(string s)
+        {
+            var delimeters = s.ToCharArray().Where(c=> !char.IsLetter(c) && !Equals(c,'\'')).Distinct().ToArray();
+            var words = s.Split(delimeters).Select(w => w.Trim().ToLowerInvariant()).Where(w => w != string.Empty).ToList();
+            Dictionary<string, int> pairs = new();
+            var uniwWords = words.Distinct().Where(w => !Enumerable.SequenceEqual(w.ToCharArray().Distinct(), "'"));
+            foreach(var word in uniwWords)
+            {
+                var countWord = words.Count(x => Equals(x, word));
+                if(pairs.Count< 3)
+                {
+                    pairs.Add(word, countWord);
+                    continue;
+                }
+
+                if (pairs.Values.Min() < countWord) pairs.Add(word, countWord);
+                if (pairs.Keys.Count > 3) pairs.Remove(pairs.FirstOrDefault(x => Equals(x.Value, pairs.Values.Min())).Key);
+            }
+            return pairs.OrderByDescending(v => v.Value).ToDictionary().Keys.ToList();
+        }
+    }
+}
